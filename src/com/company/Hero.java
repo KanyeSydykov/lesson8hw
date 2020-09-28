dc667b5e7a37bec2316cf2aaf1a3fc718afeb89d
MamkinPokemon <skantoro@inbox.ru>
2020-09-28 21:09:36 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public abstract class Hero implements HavingSuperAbility {
+    @Override
+    public String applySuperAbility() {
+        return null;
+    }
+
+
+
+
+    private int heal;
+private int damage;
+private String attackType;
+
+
+
+
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.awt.event.HierarchyBoundsListener;
+
+public class Warrior  extends  Hero implements HavingSuperAbility {
+
+
+
+
+}
